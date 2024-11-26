2adc5025901b329f3d534227469ae83d93b219a8
Challnet <challnet@mail.ru>
2024-11-26 15:12:44 +0800

    Ready for Deployment

---
+++ src/util.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export const useLocalStorage = (key, defaultValue) => {
+    const [value, setValue] = useState(() => {
+        const currentValue = localStorage.getItem(key);
+        if (currentValue) {
+            return JSON.parse(currentValue)
+        }
+        else {
+            return defaultValue;
+        }
+    });
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [value]);
+    return [value, setValue];
+};
+
+export const API_BASE_URL = 'http://localhost:8000/api/v1';
