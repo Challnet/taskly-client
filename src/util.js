daf93195cb796601d3ef63c3780b052e27a04365
Challnet <challnet@mail.ru>
2024-12-07 11:54:49 +0800

    f

--- src/util.js
+++ src/util.js
@@ -16,4 +16,4 @@ export const useLocalStorage = (key, defaultValue) => {
     return [value, setValue];
 };
 
-export const API_BASE_URL = 'https://taskly-server-zeta.up.railway.app/api/v1';
+export const API_BASE_URL = 'https://taskly-server-zeta.up.railway.app';
